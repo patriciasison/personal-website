4ed2f5f33cb06ceb036a057e1f68caee76dadccc
Patricia Sison <patricia.sison@phitopolis.com>
2020-08-23 10:40:30 +0800

    fix attempt for FOUT using gatsby browser and ssr config files

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,27 +3,17 @@ import { createGlobalStyle, ThemeProvider } from "styled-components"
 import theme from "./src/bits/theme"
 
 const GlobalStyle = createGlobalStyle`
-  html {
-    @media screen and (max-width: ${theme.breakpoint.tablet.media}px) {
-      font-size: 8px;
-    }
+html {
+  font-size: 8px;
 
-    @media screen and (min-width: ${theme.breakpoint.tablet.media}px) {
-      font-size: 8px;
-    }
-
-    @media screen and (min-width: ${theme.breakpoint.desktop.media}px) {
-      font-size: 10px;
-    }
+  @media screen and (min-width: ${theme.breakpoint.desktop.media}px) {
+    font-size: 10px;
   }
-
-  @font-face {
-    font-display: block;
-  }
+}
 
   body {
     background-color: ${theme.color.white.dark};
-    font-family: "Roboto";
+    font-family: "Roboto, sans-serif";
     margin: 0;
   }
 `
