652aa02cd2d811b301004dcdb2de7c18e4c65bf3
Patricia Sison <patricia.sison@phitopolis.com>
2020-08-23 11:22:33 +0800

    fix attempt for FOUT using external css file

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,35 +1,8 @@
 import React from "react"
-import { createGlobalStyle, ThemeProvider } from "styled-components"
+import { ThemeProvider } from "styled-components"
+import "./global-styles.css"
 import theme from "./src/bits/theme"
 
-const GlobalStyle = createGlobalStyle`
-  html {
-    @media screen and (max-width: ${theme.breakpoint.tablet.media}px) {
-      font-size: 8px;
-    }
-
-    @media screen and (min-width: ${theme.breakpoint.tablet.media}px) {
-      font-size: 8px;
-    }
-
-    @media screen and (min-width: ${theme.breakpoint.desktop.media}px) {
-      font-size: 10px;
-    }
-  }
-
-  @font-face {
-    font-display: block;
-  }
-
-  body {
-    background-color: ${theme.color.white.dark};
-    font-family: "Roboto";
-    margin: 0;
-  }
-`
-export const wrapRootElement = ({ element }) => (
-  <ThemeProvider theme={theme}>
-    <GlobalStyle />
-    {element}
-  </ThemeProvider>
-)
+export const wrapRootElement = ({ element }) => {
+  return <ThemeProvider theme={theme}>{element}</ThemeProvider>
+}
