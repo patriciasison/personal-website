b0b5d1c4d702e1b2e4bdc63bf40506f4cf946d75
Patricia Sison <patricia.sison@phitopolis.com>
2020-09-22 15:27:25 +0800

    Revert "added metadata"
    This reverts commit 4b4a47fb1a9e170916ecbe10db8b9bed8c8fdfa1.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -12,38 +12,6 @@ export const wrapRootElement = ({ element }) => (
     <Helmet>
       <title>{SITE_NAME}</title>
       <link rel="icon" href={favicon} />
-      <meta name="title" content="Patricia Sison - Software Engineer" />
-      <meta
-        name="description"
-        content="This personal website is made by Patricia herself, an experienced full-stack web developer."
-      />
-
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://patriciasison.me/" />
-      <meta property="og:title" content="Patricia Sison - Software Engineer" />
-      <meta
-        property="og:description"
-        content="This personal website is made by Patricia herself, an experienced full-stack web developer."
-      />
-      <meta
-        property="og:image"
-        content="/src/assets/vector-web-development.png"
-      />
-
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://patriciasison.me/" />
-      <meta
-        property="twitter:title"
-        content="Patricia Sison - Software Engineer"
-      />
-      <meta
-        property="twitter:description"
-        content="This personal website is made by Patricia herself, an experienced full-stack web developer."
-      />
-      <meta
-        property="twitter:image"
-        content="/src/assets/vector-web-development.png"
-      />
     </Helmet>
     <ThemeProvider theme={theme}>
       <ViewportProvider>{element}</ViewportProvider>
